d8a0d5a59fbbee179f7872f85959fdc72f66210d
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-09-29 14:40:28 +0100

    add logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,33 @@
+import {
+  BigText,
+  Container,
+  FirstPage,
+  FirstText,
+  Logo,
+  LogoWrap,
+  SmallText,
+} from "./Home.styles";
+import tetelogo from "../assets/tetelogo.svg";
+
 const Home = () => {
-    return (
-        <>
-        <div>hello</div>
-        </>
-    )
-}
+  return (
+    <>
+      <Container>
+        <LogoWrap>
+          <Logo src={tetelogo} alt="" />
+        </LogoWrap>
+        <FirstPage>
+          <FirstText>
+            <BigText>Get More Done with whitepace</BigText>
+            <SmallText>
+              Project management software that enables your teams to
+              collaborate, plan, analyze and manage everyday tasks
+            </SmallText>
+          </FirstText>
+          
+        </FirstPage>
+      </Container>
+    </>
+  );
+};
 export default Home;
---
+++ src/components/Home.styles.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+// background: #fffff;
+`;
+export const LogoWrap = styled.div`
+width: 90px;
+height: 70px;
+background: #043873;
+margin: 35px 0 0 55.5px;
+`;
+export const Logo = styled.img`
+width: 90px;
+height: 70px;
+`;
+export const FirstPage = styled.div`
+margin-top: 29px;
+border: 1px solid #043873;
+height: 829px;
+display: flex;
+`;
+export const FirstText = styled.div`
+margin: 234px 0 0 220px;
+color: #4F9CF9;
+`;
+export const BigText = styled.div`
+font-size: 64px;
+font-weight: 700;
+line-height: 77.45px;
+width: 656px;
+height: 154px;
+
+`;
+export const SmallText = styled.div`
+margin-top: 24px;
+font-size: 18px;
+font-weight: 400;
+line-height: 30px;
+width: 656px;
+height: 60px;
+
+`;
