3a3d18b6cd3ab4fb6102bc093e8228e1ae4546d4
chayilDeborah <aniyikayeolamide3@gmail.com>
2022-09-30 19:30:49 +0100

    create home page styles

--- src/components/Home.styles.jsx
+++ src/components/Home.styles.jsx
@@ -15,20 +15,20 @@ height: 70px;
 `;
 export const FirstPage = styled.div`
 margin-top: 29px;
-border: 1px solid #043873;
-height: 829px;
+// border: 1px solid #043873;
+height: 719px;
 display: flex;
 `;
 export const FirstText = styled.div`
-margin: 234px 0 0 220px;
+margin: 150px 0 0 175px;
 color: #4F9CF9;
 `;
 export const BigText = styled.div`
 font-size: 64px;
 font-weight: 700;
-line-height: 77.45px;
+// line-height: 77.45px;
 width: 656px;
-height: 154px;
+// height: 154px;
 
 `;
 export const SmallText = styled.div`
@@ -39,4 +39,188 @@ line-height: 30px;
 width: 656px;
 height: 60px;
 
+`;
+export const FirstImageDiv = styled.div`
+margin-top: 120px;
+height: 449px;
+width: 724px;
+background: #C4DEFD;
+
+`;
+export const SecondPage = styled.div`
+// border: 1px solid red;
+height: 547px;
+display: flex;
+`;
+export const SecondText = styled.div`
+color: #212529;
+margin: 0 60px 0 175px;
+`;
+export const SecondBigText = styled.div`
+margin-top: 70px;
+font-size: 72px;
+font-weight: 700;
+line-height: 87.14px;
+width: 672px;
+height: 174px;
+`;
+export const SecondSmallText = styled.div`
+width: 672px;
+height: 90px;
+font-size: 18px;
+font-weight: 400;
+line-height: 30px;
+margin-top: 24px;
+`;
+export const SecondImageDiv = styled.div`
+width: 748px;
+height: 547px;
+background: #C4DEFD;
+`;
+export const ThirdPage = styled.div`
+margin: 100px 0 0 175px;
+display: flex;
+`;
+export const ThirdImageDiv = styled.div`
+width: 661px;
+height: 561px;
+background: #C4DEFD;
+`;
+export const ThirdText = styled.div`
+color: #212529;
+margin-left: 114px;
+
+`;
+export const ThirdBigText = styled.div`
+font-size: 72px;
+font-weight: 700;
+line-height: 87.14px;
+width: 670px;
+height: 87px;
+margin-top: 183px;
+`;
+export const ThirdSmallText = styled.div`
+font-weight: 400;
+font-size: 18px;
+line-height: 30px;
+width: 670px;
+height: 60px;
+`;
+export const ForthPage = styled.div`
+margin-top: 140px;
+height: 479px;
+display: flex;
+`;
+export const FifthDiv = styled.div`
+margin-top: 140px;
+background: #043873;
+height: 574px;
+position: relative;
+display: flex;
+`;
+export const GroupSvg = styled.img`
+position: absolute;
+color: #A7CEFC;
+top: -175px;
+opacity: 0.3;     
+`;
+export const FifthText = styled.div`
+margin-left: 428.5px;
+`;
+export const FifthBigText = styled.div`
+font-weight: 700;
+font-size: 72px;
+line-height: 87px;
+color: #FFFFFF;
+margin-top: 140px;
+margin-bottom: 24px;
+`;
+export const FifthSmallText = styled.div`
+width: 1064px;
+height: 60px;
+font-weight: 400;
+font-size: 18px;
+line-height: 30px;
+color: #FFFFFF;
+`;
+export const SixthDiv = styled.div`
+display: flex;
+`;
+export const SixthText = styled.div`
+margin-left: 222px;
+`;
+export const SixthBigText = styled.div`
+margin: 160px 0 24px 0;
+font-weight: 700;
+font-size: 72px;
+line-height: 87px;
+`;
+export const SixthSmallText = styled.div`
+font-weight: 400;
+font-size: 18px;
+line-height: 30px;
+width: 800px;
+height: 90px;
+margin-bottom: 221px;
+`;
+export const LastDiv = styled.div`
+background: #043873;
+height: 848.8px;
+`;
+export const SeventhDiv = styled.div`
+display: flex;
+margin-bottom: 90px;
+`;
+export const SeventhImage = styled.div`
+width: 582px;
+height: 470.8px;
+background: #C4DEFD;
+margin: 140px 100px 0 220px;
+`;
+export const SeventhText = styled.div`
+width: 798px;
+`;
+export const SeventhBigText = styled.div`
+font-weight: 700;
+font-size: 72px;
+line-height: 87px;
+margin: 141.4px 0 24px 0;
+color: #FFFFFF;
+height: 261px;
+`;
+export const SeventhSmallText = styled.div`
+font-weight: 400;
+font-size: 18px;
+line-height: 30px;
+color: #FFFFFF;
+height: 60px;
+`;
+export const LineBreak = styled.hr`
+opacity: 0.3;
+`;
+export const IconsDiv = styled.div`
+text-align: center;
+padding-top: 24.5px;
+`;
+export const IconsText = styled.div`
+color: #FFFFFF;
+font-weight: 700;
+font-size: 60px;
+
+`;
+export const Icons = styled.div`
+`;
+export const Twitter = styled.a`
+color: rgb(29, 155, 240);
+font-size: 50px;
+&:hover {
+    color: #E28F1D;
+  }
+`;
+export const Discord = styled.a`
+color: #8ab4f8;
+font-size: 70px;
+&:hover {
+    color: #E28F1D;
+  }
 `;
